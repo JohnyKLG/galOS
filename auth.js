481b2f1ec59454025cdb928cef25e5572ed28ca3
Jonathan Vieira <johnyklg@outlook.com>
2020-07-24 17:39:44 -0300

    desenvolvendo auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+function gAuth(){
+    var dstl = 'position:absolute;top:50%;left:50%;width:350px;height:200px;';
+        dstl+= 'margin:-100px -175px;border-radius:9px;background:#aaaaaabb;';
+        dstl+= 'box-shadow: 0px 0px 10px #333;';
+    var div = document.createElement('div');div.style = dstl;div.id = 'auth';bd.appendChild(div);
+}
