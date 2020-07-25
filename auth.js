5f542c07dbff7000ba38009595a9f8e19672dab1
Jonathan Vieira <johnyklg@outlook.com>
2020-07-25 12:19:57 -0300

    desenvolvendo auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,12 @@
 function gAuth(){
-    // Definiar e criar popup de autenticação
-    var logo = 'position:absolute;bottom:30px;right:30px;width:100px;height:167px;background:url('+lg+');filter:invert(1)';
-    var lgo = document.createElement('logo');lgo.style = logo;lgo.id = 'logo';bd.appendChild(lgo);
-    var dstl = 'position:absolute;top:50%;left:50%;';
-        dstl+= 'border-radius:9px;background:#cccccccc;';
-        dstl+= 'box-shadow: 0px 0px 10px #333;font:13px sans-serif;padding:9px';
-    var div = document.createElement('div');div.style = dstl;div.id = 'auth';bd.appendChild(div);
-    var auth = document.getElementById('auth');
+    insertG('logo',100,167,0,0,30,30);
     // Verificar se há usuário
-    if(localStorage.getItem('user')){        
-        auth.style.width = '350px';        
-        auth.style.height = '200px';
+    if(localStorage.getItem('user')){
+        insertG('auth',350,200,50,50,0,0);
         auth.style.margin = '-100px -175px';
         auth.innerHTML = '<h2>Olá, benvindo ao galOS</h2>';
     }else{
-        auth.style.width = '300px';        
-        auth.style.height = '120px';
+        insertG('auth',300,120,50,50,0,0);
         auth.style.margin = '-60px -150px';
         auth.innerHTML = '<h2>Olá, benvindo ao galOS</h2>';
         auth.innerHTML += '<p>No momento não encontrei nenhum usuário.</p>';
