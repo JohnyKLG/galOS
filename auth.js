f4d283192b388786c5da71777c831c2a891fe7ba
Jonathan Vieira <johnyklg@outlook.com>
2020-07-25 17:22:39 -0300

    desenvolvendo auth.js

--- auth.js
+++ auth.js
@@ -1,15 +1,23 @@
 function gAuth(){
-    insertG('logo',100,167,0,0,30,30);
+    insertG('logo',100,167,0,0,30,30);    
+    bd.style.background = "url('"+aw+"') no-repeat center top fixed";
+    bd.style.backgroundSize = 'cover';
+    bd.style.color = "#333";
     // Verificar se há usuário
     if(localStorage.getItem('user')){
         insertG('auth',350,200,50,50,0,0);
         auth.style.margin = '-100px -175px';
         auth.innerHTML = '<h2>Olá, benvindo ao galOS</h2>';
     }else{
-        insertG('auth',300,120,50,50,0,0);
-        auth.style.margin = '-60px -150px';
-        auth.innerHTML = '<h2>Olá, benvindo ao galOS</h2>';
-        auth.innerHTML += '<p>No momento não encontrei nenhum usuário.</p>';
-        auth.innerHTML += '<button>Cadastrar usuário</button>';
+        var stl = document.createElement('style');
+        stl.innerHTML = "#btn{width:100%;height:30px;border:1px solid #666;background:#bbbbbb55;border-radius:5px;outline:none;}";
+        stl.innerHTML+= "#btn:hover{border:1px solid #577;background:#00808030;color:#355;cursor:pointer;}";
+        hd.appendChild(stl);
+
+        insertG('auth',144,120,50,50,0,0);
+        auth.style.margin = '-60px -72px';
+        auth.innerHTML = '<h2>Olá, tudo bem?</h2>';
+        auth.innerHTML += '<p style="line-height:7px">Tudo pronto para iniciar.</p><br>';
+        auth.innerHTML += '<button id="btn" onclick="gUser(\'new\')">Iniciar</button>';
     }
 }
