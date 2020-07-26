e440c3566a249d1d014afb97a1296e1cb082b03a
Jonathan Vieira <johnyklg@outlook.com>
2020-07-26 09:24:22 -0300

    desenvolvendo gen.js

--- gen.js
+++ gen.js
@@ -1,15 +1,19 @@
-function insertG(obj,w,h,t,l,b,r){
+function insertG(obj,id,w,h,t,l,b,r){
     // Objeto gráfico logo
     if(obj === 'logo'){
         var logo = 'position:absolute;bottom:'+b+'px;right:'+r+'px;width:'+w+'px;height:'+h+'px;background:url('+lg+');filter:invert(1)';
-        var lgo = document.createElement('logo');lgo.style = logo;lgo.id = 'logo';bd.appendChild(lgo);
+        var lgo = document.createElement('logo');lgo.style = logo;lgo.id = id;bd.appendChild(lgo);
     }
-    // Objeto gráfico popup do auth
-    if(obj === 'auth'){
+    // Objeto gráfico popup
+    if(obj === 'popup'){
         var dstl = 'position:absolute;top:'+t+'%;left:'+l+'%;width:'+w+'px;height:'+h+'px;';
-            dstl+= 'border-radius:9px;background:#cccccccc;';
+            dstl+= 'border-radius:9px;background:#cccccccc;color:#333;';
             dstl+= 'box-shadow: 0px 0px 10px #333;font:13px sans-serif;padding:9px';
-        var div = document.createElement('div');div.style = dstl;div.id = 'auth';bd.appendChild(div);
-        auth = document.getElementById('auth');
+        var div = document.createElement('div');div.style = dstl;div.id = id;bd.appendChild(div);
     }
+}
+function genClear(){
+    bd.innerHTML = "";
+    bd.style.font = "";
+    bd.style.color = "";
 }
