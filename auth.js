cc2b651a37107b0c24ae5ea7343af96a90c7db64
Jonathan Vieira <johnyklg@outlook.com>
2020-07-26 15:22:28 -0300

    desenvolvendo auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,8 @@
-async function gAuth(){
+function gAuth(){
+    var stl = document.createElement('style');
+    stl.innerHTML = "#btn{width:130px;height:30px;border:1px solid #666;background:#bbbbbb55;border-radius:5px;outline:none;}";
+    stl.innerHTML+= "#btn:hover{box-shadow: 0px 0px 5px #577;border:1px solid #577;background:#eeeeee40;color:#355;cursor:pointer;}";
+    hd.appendChild(stl);
     insertG('logo','logo',100,167,0,0,30,30);    
     bd.style.background = "url('"+aw+"') no-repeat center top fixed";
     bd.style.backgroundSize = 'cover';
@@ -8,13 +12,9 @@ async function gAuth(){
         auth.style.margin = '-100px -175px';
         auth.innerHTML = '<h2>Olá, benvindo ao galOS</h2>';
     }else{
-        var stl = document.createElement('style');
-        stl.innerHTML = "#btn{width:130px;height:30px;border:1px solid #666;background:#bbbbbb55;border-radius:5px;outline:none;}";
-        stl.innerHTML+= "#btn:hover{border:1px solid #577;background:#00808030;color:#355;cursor:pointer;}";
-        hd.appendChild(stl);
-        insertG('popup','auth',270,130,50,50,0,0);
+        insertG('popup','auth',270,135,50,50,0,0);
         auth = document.getElementById('auth');
-        auth.style.margin = '-65px -135px';
+        auth.style.margin = '-67px -135px';
         auth.innerHTML = '<h2>Olá, tudo bem?</h2>';
         auth.innerHTML += '<span>Não identificamos nenhum usuário cadastrado neste host, o que você gostaria de fazer?</span><br><br>';
         auth.innerHTML += '<button id="btn" onclick="gUser(\'new\')" style="margin-right:10px">Cadastrar usuário</button>';
@@ -25,7 +25,13 @@ function gUser(ins){
     genClear();
     insertG('logo','logo',100,167,0,0,30,30);
     if(ins === 'new'){
-        //cad
+        insertG('popup','pnew',360,400,50,50,0,0);
+        pnew = document.getElementById('pnew');
+        pnew.style.margin = '-200px -180px';
+        pnew.innerHTML = '<h3>Olá, tudo bem?</h3>';
+        pnew.innerHTML += '<span>Não identificamos nenhum usuário cadastrado neste host, o que você gostaria de fazer?</span><br><br>';
+        pnew.innerHTML += '<button id="btn" onclick="gUser(\'new\')" style="margin-right:10px">Cadastrar usuário</button>';
+        pnew.innerHTML += '<button id="btn" onclick="gUser(\'imp\')">Importar usuário</button>';
     }else if(ins === 'imp'){
         //imp
     }
