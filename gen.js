6069b508bfc3a96751a75d26de002056c8bb1faf
Jonathan Vieira <johnyklg@outlook.com>
2020-07-26 15:22:50 -0300

    desenvolvendo gen.js

--- gen.js
+++ gen.js
@@ -1,3 +1,24 @@
+document.addEventListener("dragstart", function( event ){
+/* ----------------- Johny KLG -----------------+
+| Plataforma gráfica de gerenciamento do galOS  |
+| baseada em object style CSS3 sendo puramente  |
+| desenvolvida em JavaScript.                   |
+|                                               |
+| Brasil, julho de 2020                         |
++--------------------------------------------- */
+    var img = new Image();
+    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+    event.dataTransfer.setDragImage(img, 0, 0);
+}, false);
+// Definir posição do objeto
+function dragG(id){    
+    eid = document.getElementById(id);
+    var e = window.event;
+    if(e.clientY >= 1){y = e.clientY}
+    if(e.clientX >= 1){x = e.clientX}
+    eid.style.top = y+"px";
+    eid.style.left = x+"px";
+}
 function insertG(obj,id,w,h,t,l,b,r){
     // Objeto gráfico logo
     if(obj === 'logo'){
@@ -7,9 +28,10 @@ function insertG(obj,id,w,h,t,l,b,r){
     // Objeto gráfico popup
     if(obj === 'popup'){
         var dstl = 'position:absolute;top:'+t+'%;left:'+l+'%;width:'+w+'px;height:'+h+'px;';
-            dstl+= 'border-radius:9px;background:#cccccccc;color:#333;';
-            dstl+= 'box-shadow: 0px 0px 10px #333;font:13px sans-serif;padding:9px';
-        var div = document.createElement('div');div.style = dstl;div.id = id;bd.appendChild(div);
+            dstl+= 'border-radius:9px;background:#cccccccd;color:#333;';
+            dstl+= 'box-shadow: 0px 0px 10px #333;font:9.5pt sans-serif;padding:0 9px';
+        var div = document.createElement('div');div.style = dstl;div.id = id;
+        div.draggable = "true";div.setAttribute('ondrag','dragG(this.id)');bd.appendChild(div);
     }
 }
 function genClear(){
