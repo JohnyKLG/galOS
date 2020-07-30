c1b4f9b145c22fed1b825077d79ae88425a54e3b
Jonathan Vieira <johnyklg@outlook.com>
2020-07-30 10:26:05 -0300

    dev auth.js

--- auth.js
+++ auth.js
@@ -9,11 +9,11 @@ function gAuth(){
         user = userA.split("*");var usr;
         if(user[2] == '0'){usr = user[0].split(" ")[0];}else{usr = user[2];}
         var stl = document.createElement('style');
-        stl.innerHTML = "#auth{margin:-102px -90px;text-align:center;}"        
-        stl.innerHTML+= "#auth h2{font-size:16pt;margin:0;}"
-        stl.innerHTML+= "#auth span{font-size:8pt;margin:0;}"
+        stl.innerHTML = "#auth{margin:-102px -90px;text-align:center;}";        
+        stl.innerHTML+= "#auth h2{font-size:16pt;margin:0;}";
+        stl.innerHTML+= "#auth span{font-size:8pt;margin:0;}";
         stl.innerHTML+= "#auth div{margin:9px 45px;width:90px;height:90px;background:url('"+user[4]+"') no-repeat;";
-        stl.innerHTML+= "background-size:90px;border:1px solid #888;border-radius:50%;}"
+        stl.innerHTML+= "background-size:90px;border:1px solid #888;border-radius:50%;}";
         stl.innerHTML+= "#auth input{width:calc(100% - 16px);padding:2px 7px;height:30px;text-align:center;";
         stl.innerHTML+= "margin:9px 0;border:1px solid #999;background:#eeeeee77;border-radius:5px;outline:none;}";
         stl.innerHTML+= "#auth input:hover{background:#eeeeeeaa;}";
@@ -23,11 +23,11 @@ function gAuth(){
         auth.innerHTML = '<div ondblclick="gUser(\'new\')"></div>';
         auth.innerHTML+= '<h2>'+usr+'</h2>';
         auth.innerHTML+= '<span>'+user[1]+'</span>';
-        auth.innerHTML+= '<input id="pw" type="password" placeholder="Digite sua senha"></input>';
+        auth.innerHTML+= '<input id="pw" type="password" placeholder="Digite sua senha" onkeyup="gLogin(this.value)"></input>';
         document.getElementById('pw').focus();
     }else{
         var stl = document.createElement('style');
-        stl.innerHTML = "#auth{margin:-67px -135px;}"
+        stl.innerHTML = "#auth{margin:-67px -135px;}";
         stl.innerHTML+= "#auth .btn{width:130px;height:30px;border:1px solid #888;background:#bbbbbb55;border-radius:5px;outline:none;}";
         stl.innerHTML+= "#auth .btn:hover{box-shadow: 0px 0px 5px #577;border:1px solid #577;background:#eeeeee40;color:#355;cursor:pointer;}";        
         hd.appendChild(stl);
@@ -39,24 +39,31 @@ function gAuth(){
         auth.innerHTML+= '<button class="btn" onclick="gUser(\'imp\')">Importar usuário</button>';
     }
 }
+function gLogin(pw){
+    if(pw === user[3]){
+        genClear();   
+        bd.style.background = "url('"+wall64+"') no-repeat center top fixed";
+        bd.style.backgroundSize = 'cover';
+    }
+}
 function gUser(ins){
     genClear();
     insertG('logo','logo',100,167,0,0,30,30);
     if(ins === 'new'){
         var stl = document.createElement('style');
-        stl.innerHTML = "#pnew{margin:-175px -150px;text-align:right;}"
-        stl.innerHTML+= "#pnew h3{float:left;}"
+        stl.innerHTML = "#pnew{margin:-175px -150px;text-align:right;}";
+        stl.innerHTML+= "#pnew h3{float:left;}";
         stl.innerHTML+= "#pnew input{width:calc(100% - 16px);padding:2px 7px;height:30px;margin:3px 0;border:1px solid #999;background:#eeeeee77;border-radius:5px;outline:none;}";
         stl.innerHTML+= "#pnew input:hover{background:#eeeeeeaa;}";
         stl.innerHTML+= "#pnew #fd0{float:left;width:calc(100% - 98px);}";
         stl.innerHTML+= "#pnew div{position:absolute;top:9px;right:9px;width:73px;height:73px;border:1px solid #888;border-radius:7px;";
-        stl.innerHTML+= "background:#eeeeee77;color:#888;text-align:center;line-height:73px;}"
-        stl.innerHTML+= "#pnew div #prev{position:absolute;top:0px;left:0px;width:100%;height:100%;margin:0;padding:0;border:0;border-radius:6px;}"
-        stl.innerHTML+= "#pnew div input{position:absolute;top:0px;left:0px;width:100%;height:100%;margin:0;padding:0;border:0;opacity:0;cursor:pointer;}"
-        stl.innerHTML+= "#pnew #gFileOut{display:none;}"
+        stl.innerHTML+= "background:#eeeeee77;color:#888;text-align:center;line-height:73px;}";
+        stl.innerHTML+= "#pnew div #prev{position:absolute;top:0px;left:0px;width:100%;height:100%;margin:0;padding:0;border:0;border-radius:6px;}";
+        stl.innerHTML+= "#pnew div input{position:absolute;top:0px;left:0px;width:100%;height:100%;margin:0;padding:0;border:0;opacity:0;cursor:pointer;}";
+        stl.innerHTML+= "#pnew #gFileOut{display:none;}";
         stl.innerHTML+= "#pnew .btn{width:100px;height:30px;border:1px solid #888;background:#bbbbbb55;border-radius:5px;outline:none;}";
         stl.innerHTML+= "#pnew .btn:hover{box-shadow: 0px 0px 5px #577;border:1px solid #577;background:#eeeeee40;color:#355;cursor:pointer;}";
-        stl.innerHTML+= "#pnew #cnl:hover{box-shadow: 0px 0px 5px #755;border:1px solid #755;color:#533;}"
+        stl.innerHTML+= "#pnew #cnl:hover{box-shadow: 0px 0px 5px #755;border:1px solid #755;color:#533;}";
         hd.appendChild(stl);
         insertG('popup','pnew',300,350,50,50,0,0);
         pnew = document.getElementById('pnew');
@@ -72,14 +79,14 @@ function gUser(ins){
         pnew.innerHTML+= '<button class="btn" onclick="gReg(\'user\')">Concluir</button><img id="gFileOut"/>';
     }else if(ins === 'imp'){
         var stl = document.createElement('style');
-        stl.innerHTML = "#pimp{margin:-91px -149px;}"
+        stl.innerHTML = "#pimp{margin:-91px -149px;}";
         stl.innerHTML+= "#pimp input{width:50px;padding:2px 7px;height:30px;margin:3px 0;";
         stl.innerHTML+= "border:1px solid #999;background:#eeeeee77;border-radius:5px;outline:none;";
         stl.innerHTML+= "text-transform:uppercase;text-align:center;font:17px sans-serif;}";
         stl.innerHTML+= "#pimp input:hover{background:#eeeeeeaa;}";
         stl.innerHTML+= "#pimp .btn{width:100px;height:30px;border:1px solid #888;background:#bbbbbb55;border-radius:5px;outline:none;float:right;}";
         stl.innerHTML+= "#pimp .btn:hover{box-shadow: 0px 0px 5px #577;border:1px solid #577;background:#eeeeee40;color:#355;cursor:pointer;}";
-        stl.innerHTML+= "#pimp #cnl:hover{box-shadow: 0px 0px 5px #755;border:1px solid #755;color:#533;}"
+        stl.innerHTML+= "#pimp #cnl:hover{box-shadow: 0px 0px 5px #755;border:1px solid #755;color:#533;}";
         hd.appendChild(stl);
         var sep = "&nbsp;-&nbsp;";
         insertG('popup','pimp',298,182,50,50,0,0);
