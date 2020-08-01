d11afdd76cb723f0fefe8784208b956e6be7add6
Jonathan Vieira <johnyklg@outlook.com>
2020-08-01 09:04:14 -0300

    dev sys.js

--- sys.js
+++ sys.js
@@ -39,8 +39,8 @@ async function galOS(ins){
         bd.innerHTML += "<p "+pc+">Carregando encoder de imagem <l "+pl+">i64.asm</l></p>";
         var s2 = document.createElement('script');s2.src = 'i64.asm';hd.appendChild(s2);
         await load(200);
-        bd.innerHTML += "<p "+pc+">Carregando decoder de imagem <l "+pl+">x64.h</l></p>";
-        var s3 = document.createElement('script');s3.src = 'x64.h';hd.appendChild(s3);
+        bd.innerHTML += "<p "+pc+">Carregando decoder de imagem <l "+pl+">x64.c</l></p>";
+        var s3 = document.createElement('script');s3.src = 'x64.c';hd.appendChild(s3);
         await load(200);
         bd.innerHTML += "<p "+pc+">Carregando gerenciador de aplicativos <l "+pl+">app.js</l></p>";
         var s4 = document.createElement('script');s4.src = 'app.js';hd.appendChild(s4);
