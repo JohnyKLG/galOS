0eb36bcb5deff3a779b6d58af7d23ac4b0569e4e
Jonathan Vieira <johnyklg@outlook.com>
2023-12-09 20:40:01 -0300

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,38 +1,40 @@
 function gReg(opr){
     if(opr == 'user'){
-        var name = document.getElementById('fd0').value;
-        var mail = document.getElementById('fd1').value;
-        var cfml = document.getElementById('fd2').value;
-        var nick = document.getElementById('fd3').value;
-        var pass = document.getElementById('fd4').value;
-        var cfpw = document.getElementById('fd5').value;
+        let name = document.getElementById('fd0').value;
+        let mail = document.getElementById('fd1').value;
+        let cfml = document.getElementById('fd2').value;
+        let nick = document.getElementById('fd3').value;
+        let pass = document.getElementById('fd4').value;
+        let cfpw = document.getElementById('fd5').value;
         if(name && mail && pass){
-            if(mail == cfml){
-                var mail = document.getElementById('fd1').value;
-                if(pass == cfpw){
-                    if(nick){nick = nick}else{nick = '0';}
-                    var str = name+"*"+mail+"*"+nick+"*"+document.getElementById('fd4').value+"*"+localStorage.getItem('pic');
-                    localStorage.setItem('user', str);
-                    localStorage.removeItem('pic');
-                    gAlert('cadOK','gAuth()');                
-                }else{gAlert('pass','0');}
-            }else{gAlert('mail','0');}
+            if(localStorage.getItem('pic')){
+                if(mail == cfml){
+                const mail = document.getElementById('fd1').value;
+                    if(pass == cfpw){
+                        if(nick){nick = nick}else{nick = '0';}
+                        let str = name+"*"+mail+"*"+nick+"*"+pass+"*"+localStorage.getItem('pic');
+                        localStorage.setItem('user', str);
+                        localStorage.removeItem('pic');
+                        gAlert('cadOK','gAuth()');                
+                    }else{gAlert('pass','0');}
+                }else{gAlert('mail','0');}
+            }else{gAlert('nopic','0');}
         }else{gAlert('empty','0');}
     }
     if(opr == 'code'){
-        var cd0 = document.getElementById('fd0').value;
-        var cd1 = document.getElementById('fd1').value;
-        var cd2 = document.getElementById('fd2').value;
-        var cd3 = document.getElementById('fd3').value;
-        var str = cd0+cd1+cd2+cd3;
-        var code = str.toUpperCase();
+        let cd0 = document.getElementById('fd0').value;
+        let cd1 = document.getElementById('fd1').value;
+        let cd2 = document.getElementById('fd2').value;
+        let cd3 = document.getElementById('fd3').value;
+        let str = cd0+cd1+cd2+cd3;
+        let code = str.toUpperCase();
         alert('code = '+code);
     }
 }
 var gRegFile = function(event){
-    var prv = document.getElementById('prev');
+    const prv = document.getElementById('prev');
     prv.src = URL.createObjectURL(event.target.files[0]);
-    var out = document.getElementById('gFileOut');
+    const out = document.getElementById('gFileOut');
     out.src = URL.createObjectURL(event.target.files[0]);
     prv.onload = function(){
         URL.revokeObjectURL(prv.src);
